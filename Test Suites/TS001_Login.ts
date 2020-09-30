3cc138439dac0746e3adfbe17f5885c6c6ae1ff5
duyvt-it <duyvt.it@gmail.com>
2020-09-30 19:53:16 +0700

    submit

---
+++ Test Suites/TS001_Login.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS001_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2fa0186e-b601-420b-b71d-461b7eef7ff9</testSuiteGuid>
+   <testCaseLink>
+      <guid>b2557a9b-a420-4803-8c26-6d08e9ada80f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC003_PasswordBlank</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0b9cf40b-da72-4620-b9e2-3539246f3bff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC004_WrongPassword</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d2415a5c-ae77-4d57-90f3-16c7dcaaf182</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC005_SuccessLogin</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>da9dd862-ae51-41a5-9ee2-c0599768c080</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC002_UsernameBlank</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>49b87b4a-2dd3-4d26-941a-02b22e1790e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001_LoginFieldBlank</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
